e94b44a33651d5266bb2d2e65f2eb6a845807f66
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-10-25 21:24:43 -0300

    pagina em criação

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import './globals.css';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-   
+      <h1>Bem-vindo ao meu site!</h1>
     </main>
   );
 }
